4500be947597cb2f8b038ca393c1b3dc05b8871f
Erick Adrian Perez <erick.perez@plansource.com>
2023-10-14 07:43:50 -0600

    Fixed webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const SRC_DIR = path.resolve(__dirname, "./src");
-const DIST_DIR = path.resolve(__dirname, "dist", "problem");
+const DIST_DIR = path.resolve(__dirname, "dist", "orbit");
 
 // compile sass into css
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -9,6 +9,16 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+// let allTemplateNames = ['index', 'web_components']; // add more template names in array if needed
+// let allHtmlTemplatePlugins = allTemplateNames.map(name => {
+//   return new HtmlWebpackPlugin({
+//     title: 'Orbit',
+//     template: `src/${name}.html`,
+//     // filename: `${name}.html`, // output HTML files
+//     // chunks: [`${name}`] // respective JS files
+//   })
+// });
 
 
 let mode = "development";
@@ -22,54 +32,38 @@ if (process.env.NODE_ENV === "production") {
 module.exports = {
   mode: mode,
   entry: {
+    // index: path.resolve(__dirname, 'src/javascripts/index.js')
     index: SRC_DIR + '/javascripts/index.js',
-    password_field_functions:  SRC_DIR + '/javascripts/field_functions.js',
-    application: SRC_DIR + '/stylesheets/application.scss',
-    bootstrap_problem: SRC_DIR + '/stylesheets/bootstrap_problem.scss',
+    password_field_functions:  SRC_DIR + '/javascripts/password_field_functions.js',
+    // application: SRC_DIR + '/stylesheets/application.scss',
+    // bootstrap_orbit: SRC_DIR + '/stylesheets/bootstrap_orbit.scss',
+    web_components: SRC_DIR + '/javascripts/web_components.js'
   },
   output: {
+    // filename: 'javascripts/[name].[chunkhash].js',
+    // path: path.resolve(__dirname, DIST_DIR),
+    // publicPath: '/orbit/'
+    path: path.resolve(__dirname, 'dist'),
     filename: 'javascripts/[name].[chunkhash].js',
-    path: path.resolve(__dirname, DIST_DIR),
-    publicPath: '/problem/'
+    clean: true,
+    assetModuleFilename: '[name][ext]'
   },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader, // Extracts CSS into separate files
-          'css-loader',               // Translates CSS into CommonJS
-          {
-            loader: 'sass-loader',   // Compiles Sass to CSS
-            options: {
-              sassOptions: {
-                includePaths: [
-                  path.resolve(__dirname, 'src'),
-                  path.resolve(__dirname, 'src/stylesheets')
-                ],
-              }
-            }
-          }
-        ]
-      },
-      {
-        test: /\.js$/, // regex to match .js files
-        exclude: /node_modules/,
-      }
-    ]
-  },
+  // optimization: {
+  //   splitChunks: {
+  //     chunks: 'all',
+  //   },
+  // },
   devtool: 'inline-source-map',
   devServer: {
-    static: path.join(__dirname, 'src'), // Specify the root directory for your static files (e.g., images, styles)
+    // static: path.join(__dirname, 'src'), // Specify the root directory for your static files (e.g., images, styles)
+    static: {
+      directory: path.resolve(__dirname, 'src')
+    },
     port: 3050, // Port for the development server
     open: true,
-    hot: false, // Enable hot module replacement
+    hot: true, // Enable hot module replacement
+    compress: true,
+    historyApiFallback: true,
     onBeforeSetupMiddleware: function(devServer) {
       devServer.app.use(function(req, res, next) {
         const allowedOrigins = [
@@ -87,15 +81,40 @@ module.exports = {
       });
     }
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+    ]
+  },
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'stylesheets/[name].[contenthash].css'
     }),
     new WebpackManifestPlugin({}),
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
+      title: 'Orbit',
+      template: 'src/index.html',
+      // inject: true,
+      chunks: ['index', 'password_field_functions', 'web_components'],
+      filename: 'index.html'
+      // filename: 'index.html' // this is for build to prod
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Orbit',
+      template: 'src/web_components.html',
+      // inject: true,
+      chunks: ['index', 'password_field_functions', 'web_components'],
+      filename: 'web_components.html'
+      // filename: 'index.html' // this is for build to prod
     }),
     new CleanWebpackPlugin(),
-  ],
+  ]
 };
 
